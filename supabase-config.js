d4954a10352739180f7b07226984f86048c7571d
rkdals0912-lang <rkdals0912@gmail.com>
2026-09-21 12:46:57 +0900

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,6 +2,6 @@
 // GitHub Pages 브라우저에서는 Publishable key만 사용하세요.
 // service_role / secret key는 절대 넣지 마세요.
 window.TOPSEAL_SUPABASE_CONFIG = {
-  url: 'https://ixpylhurdtytzbdxslcn.supabase.co/rest/v1/',
+  url: 'https://ixpylhurdtytzbdxslcn.supabase.co/',
   publishableKey: 'sb_publishable_LR-tApZSWHpezl-vhb4MHA_QsLShem_'
 };
